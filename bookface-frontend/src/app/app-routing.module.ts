9a7c9230927039d7c9f7ab56b5c3d2b1352a481a
Zerneax <d.chris59@gmail.com>
2018-12-01 18:24:30 +0100

    finalisation app.component v2

--- bookface-frontend/src/app/app-routing.module.ts
+++ bookface-frontend/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import {SignInComponent} from './sign-in/sign-in.component';
 
 const routes: Routes = [
-  { path: 'signIn', component: SignInComponent}
+  { path: '', component: SignInComponent}
 ];
 
 @NgModule({
