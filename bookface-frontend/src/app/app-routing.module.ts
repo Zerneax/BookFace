fdcf11339790ea513fbd9b0da45eaf79da33a539
Zerneax <d.chris59@gmail.com>
2018-12-01 15:58:47 +0100

    Add dependencies semantic and fontAwesome

--- bookface-frontend/src/app/app-routing.module.ts
+++ bookface-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {SignInComponent} from './sign-in/sign-in.component';
+
+const routes: Routes = [
+  { path: 'signIn', component: SignInComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
