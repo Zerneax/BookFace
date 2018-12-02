1bc9d1c2b493e928971389b97d1185ff2fff11da
Zerneax <d.chris59@gmail.com>
2018-12-02 11:10:11 +0100

    end of signin

--- bookface-frontend/src/app/sign-in/sign-in.component.ts
+++ bookface-frontend/src/app/sign-in/sign-in.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { SignInService } from './../services/signIn/sign-in.service';
 import { User } from './../models/user/user';
+
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-sign-in',
@@ -27,9 +29,23 @@ export class SignInComponent implements OnInit {
       firstName: ['', Validators.required],
       mail: ['', [Validators.required, Validators.pattern(/^([a-zA-Z0-9._-]+)@([a-zA-Z]*)[.{1}]([a-z]*)$/)]],
       password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(12)]],
-      birthday: ['', Validators.required],
+      birthday: ['', [Validators.required, this.checkBirthday]],
       gender: ['', Validators.required],
     })
+  }
+
+  checkBirthday(control: FormControl) {
+    if(control.value != "") {
+      let dateSelected = moment(control.value);
+      if(dateSelected.isAfter(moment().subtract(13, 'years'))) {
+        return {
+          'isUnder13' : true
+        }
+      }
+    }
+    return {
+      'isUnder13' : false
+    }
   }
 
   createAccount() {
