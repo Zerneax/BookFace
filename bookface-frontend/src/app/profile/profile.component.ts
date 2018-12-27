5e91cdc9ae309b6fc5c4508b5069725db218097b
Zerneax <d.chris59@gmail.com>
2018-12-27 20:34:41 +0100

    update profile

--- bookface-frontend/src/app/profile/profile.component.ts
+++ bookface-frontend/src/app/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from './../services/auth/auth.service';
 import { User } from '../models/user/user';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-profile',
@@ -12,12 +13,17 @@ import { Router } from '@angular/router';
 export class ProfileComponent implements OnInit {
 
   currentUser = new User();
+  birthday: Date;
 
   constructor(private authService: AuthService,
     private router: Router) { }
 
   ngOnInit() {
     this.currentUser = this.authService.getCurrentUser();
+    this.birthday = new Date(this.currentUser.birthday);
+    // console.log(moment(this.currentUser.birthday, "yyyy-MM-ddTHH:mm:sssz").format());
+    // this.birthday = new Date(this.currentUser.birthday);//moment.parseZone(this.currentUser.birthday).toDate();
+    // this.birthday = moment(this.currentUser.birthday, "yyyy-MM-dd")..toDate();
   }
 
   save() {
