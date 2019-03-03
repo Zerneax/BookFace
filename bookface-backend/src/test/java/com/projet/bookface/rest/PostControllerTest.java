76836d6edc25e6c041a8e9bcd41e9ffc3322138c
Zerneax <d.chris59@gmail.com>
2019-03-03 21:50:17 +0100

    [Test] refacotr PostControllerTest

--- bookface-backend/src/test/java/com/projet/bookface/rest/PostControllerTest.java
+++ bookface-backend/src/test/java/com/projet/bookface/rest/PostControllerTest.java
@@ -1,17 +1,14 @@
 package com.projet.bookface.rest;
 
 import com.projet.bookface.controller.PostController;
+import com.projet.bookface.dao.PostDao;
 import com.projet.bookface.models.Post;
 import com.projet.bookface.odt.PostOdt;
 import org.junit.Before;
-import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -24,72 +21,92 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class PostControllerTest {
 
-    @Autowired
     private PostController postController;
-    @Autowired
-    private MongoTemplate mongoTemplate;
-
-    private String idPost;
+    private PostDao postDao;
 
 
     @Before
     public void setUp(){
-        this.mongoTemplate.remove(new Query(), Post.class);
-        this.stage1_createPost();
+       this.postDao = Mockito.mock(PostDao.class);
+
+       this.postController = new PostController(this.postDao);
     }
 
 
     @Test
-    public void stage1_createPost() {
-        PostOdt postOdt = mockPostOdt();
+    public void createPost() {
 
-        ResponseEntity response = this.postController.createPost(postOdt);
+        Mockito.when(this.postDao.createPost(Mockito.any())).thenReturn(mockPost());
+
+        ResponseEntity response = this.postController.createPost(mockPostOdt());
         assertEquals(HttpStatus.CREATED, response.getStatusCode());
         URI uri = response.getHeaders().getLocation();
         assertNotEquals("", uri);
 
         String[] splitUri = uri.toString().split("/");
-        this.idPost = splitUri[splitUri.length -1];
+        assertEquals("123456789", splitUri[splitUri.length -1]);
     }
 
     @Test
-    public void stage2_getPost() {
-        ResponseEntity response = this.postController.getPost(this.idPost);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-
-        PostOdt postOdt = (PostOdt) response.getBody();
-        assertNotNull(postOdt);
-    }
-
-    @Test
-    public void stage3_getAllPosts() {
+    public void getAllPosts_ok() {
+        Mockito.when(this.postDao.findAllPostByAuthor(Mockito.anyString())).thenReturn(mockPosts());
         ResponseEntity response = this.postController.getAllPost("test");
         assertEquals(HttpStatus.OK, response.getStatusCode());
 
         List<PostOdt> posts = (List<PostOdt>) response.getBody();
-        assertNotNull(posts);
+        assertEquals(2, posts.size());
     }
+
+    @Test
+    public void getAllPosts_notfound() {
+        Mockito.when(this.postDao.findAllPostByAuthor(Mockito.anyString())).thenReturn(null);
+        ResponseEntity response = this.postController.getAllPost("test");
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    @Test
+    public void getPost_ok() {
+        Mockito.when(this.postDao.findPost(Mockito.anyString())).thenReturn(mockPost());
+        ResponseEntity response = this.postController.getPost("any");
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        PostOdt postOdt = (PostOdt) response.getBody();
+        assertNotNull(postOdt);
+        assertEquals(mockPost(), postOdt.getPost());
+    }
+
+    @Test
+    public void getPost_nofound() {
+        Mockito.when(this.postDao.findPost(Mockito.anyString())).thenReturn(null);
+        ResponseEntity response = this.postController.getPost("any");
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
 
     @Test
     public void stage4_updateLike() {
-        ResponseEntity response = this.postController.updateLike(this.idPost, "me");
+        Mockito.when(this.postDao.findPost(Mockito.anyString())).thenReturn(mockPost());
+        ResponseEntity response = this.postController.updateLike("any", "me");
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
     @Test
-    public void stage5_deletePost() {
-        ResponseEntity response = this.postController.deletePost(this.idPost);
+    public void deletePost() {
+        ResponseEntity response = this.postController.deletePost("any");
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
-    @Test
-    public void getPost_notFound() {
-        ResponseEntity response = this.postController.getPost("");
-        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
 
+
+    protected Post mockPost() {
+        return Post.builder()
+                .id("123456789")
+                .author("test")
+                .content("content")
+                .like(new ArrayList<>())
+                .build();
     }
 
     protected PostOdt mockPostOdt() {
@@ -104,5 +121,25 @@ public class PostControllerTest {
                 .post(post)
                 .build();
 
+    }
+
+    protected List<Post> mockPosts() {
+        List<Post> liste = new ArrayList<>();
+
+        liste.add(Post.builder()
+                .id("123456789")
+                .author("test")
+                .content("content")
+                .like(new ArrayList<>())
+                .build());
+
+        liste.add(Post.builder()
+                .id("987654321")
+                .author("test")
+                .content("content")
+                .like(new ArrayList<>())
+                .build());
+
+        return liste;
     }
 }
