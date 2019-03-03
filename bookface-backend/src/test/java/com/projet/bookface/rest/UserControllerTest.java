e5e17b448300a9edf40cc85609e971b73d8e63b3
Zerneax <d.chris59@gmail.com>
2019-03-03 21:37:20 +0100

    fix runner

--- bookface-backend/src/test/java/com/projet/bookface/rest/UserControllerTest.java
+++ bookface-backend/src/test/java/com/projet/bookface/rest/UserControllerTest.java
@@ -21,6 +21,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -34,7 +35,7 @@ import com.projet.bookface.models.User;
 import com.projet.bookface.odt.MailAvailableOdt;
 import com.projet.bookface.odt.UserOdt;
 
-@RunWith(SpringRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 public class UserControllerTest {
 
