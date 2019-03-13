f2489c435d2d4e7d3b5d9b7af72eaac204924d40
Zerneax <d.chris59@gmail.com>
2019-03-13 20:29:55 +0100

    [test] end of post.component.spec

--- bookface-frontend/src/app/pages/post/post.component.spec.ts
+++ bookface-frontend/src/app/pages/post/post.component.spec.ts
@@ -21,10 +21,16 @@ import { ProfileModule } from '../profile/profile.module';
 import { PeopleModule } from '../people/people.module';
 import { ErrorModule } from '../error/error.module';
 import { APP_BASE_HREF } from '@angular/common';
+import { PostService } from 'src/app/services/post/post.service';
+import { of, throwError } from 'rxjs';
+import { ErrorService } from 'src/app/services/error/error.service';
 
 describe('PostComponent', () => {
   let component: PostComponent;
   let fixture: ComponentFixture<PostComponent>;
+  let postService: PostService;
+  let authService: AuthService;
+  let errorService: ErrorService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -63,13 +69,73 @@ describe('PostComponent', () => {
     post.like = [];
     component.post = post;
 
-    const authService = fixture.debugElement.injector.get(AuthService);
+    authService = fixture.debugElement.injector.get(AuthService);
     spyOn(authService, 'getCompleteNameOfCurrentUser').and.returnValue("test test");
+
+    errorService = fixture.debugElement.injector.get(ErrorService);
+    spyOn(errorService, 'displayErrorMessage').and.callThrough();
+
+    postService = TestBed.get(PostService);
 
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should test like OK', () => {
+    let post: Post = new Post();
+    post.author = "test";
+    post.content = "content";
+    post.date = "10/10/1980";
+    post.id = "1";
+    post.like = [];
+
+    spyOn(postService, 'like').and.callFake(() => {return of(post);});
+
+    component.like();
+
+    expect(authService.getCompleteNameOfCurrentUser).toHaveBeenCalled();
+    expect(postService.like).toHaveBeenCalled();
+    expect(component.post).not.toBeUndefined();
+  });
+
+  it('should test like KO', () => {
+    let post: Post = new Post();
+    post.author = "test";
+    post.content = "content";
+    post.date = "10/10/1980";
+    post.id = "1";
+    post.like = [];
+
+    spyOn(postService, 'like').and.callFake(() => {return throwError('erreur');});
+
+    component.like();
+
+    expect(authService.getCompleteNameOfCurrentUser).toHaveBeenCalled();
+    expect(postService.like).toHaveBeenCalled();
+    expect(errorService.displayErrorMessage).toHaveBeenCalled();
+  });
+
+  it('should test delete', () => {
+
+    spyOn(postService, 'delete').and.callFake(() => {});
+
+    component.delete();
+    expect(postService.delete).toHaveBeenCalled();
+  });
+
+  it('should test disableLike true', () => {
+    let array = ['test test'];
+    component.post.like = array;
+    let retour = component.disabledLike();
+    expect(retour).toBeTruthy();
+  });
+
+  it('should test disableLike false', () => {
+    component.post.like = [];
+    let retour = component.disabledLike();
+    expect(retour).toBeFalsy();
   });
 });
